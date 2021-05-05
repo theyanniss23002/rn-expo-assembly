fd054d2b3e30c85bed24faa8f00c7d3c01ce001f
theyanniss23002 <mr.artur23002@gmail.com>
2021-05-05 14:03:40 +0300

    Structure improvement

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const App = () => {
+const App: React.FC = () => {
     return (
         <View>
             <Text>Hello</Text>
